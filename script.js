72ae7f6913801bdde4a9a7078423721988b6d4b1
Demetriusz <znaten@gmail.com>
2019-12-08 19:49:44 +0300

    update freq and design, refactoring

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
+let cols, rows, cellSize;
 let audio = new Audio();
 audio.src = 'Limp Bizkit - Take A Look Around.mp3';
 audio.controls = true;
 
 window.onload = initMp3Player;
-
-let cols = document.getElementById('nCols').value;
-let rows = document.getElementById('nRows').value;
-let cellSize = Math.floor(1350 / cols);
 
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
@@ -14,13 +11,11 @@ let ctx = canvas.getContext('2d');
 const audioCtx = new window.AudioContext();
 const source = audioCtx.createMediaElementSource(audio);
 const analyser = audioCtx.createAnalyser();
-analyser.fftSize = 512;
+analyser.fftSize = 2048;
 const dataArray = new Uint8Array(analyser.frequencyBinCount);
 
 analyser.connect(audioCtx.destination);
 source.connect(analyser);
-
-
 
 //init
 function initMp3Player() {
@@ -28,7 +23,7 @@ function initMp3Player() {
     draw();
 }
 
-//получение файла с локальной машины
+//получение файла с локального диска
 function uploadFile(files) {
     let file = files[0];
     let info = document.getElementById('musicInfo');
@@ -41,28 +36,36 @@ function uploadFile(files) {
         audio.load();
     };
 }
- 
+
 //отрисовка сетки по параметрам
 function draw() {
     requestAnimationFrame(draw);
     analyser.getByteFrequencyData(dataArray);
-    ctx.clearRect(0, 0, canvas.width, canvas.height); 
+
+    cols = document.getElementById('nCols').value;
+    rows = document.getElementById('nRows').value;
+    cellSize = Math.floor(1350 / cols);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    let cols = document.getElementById('nCols').value;
-    let rows = document.getElementById('nRows').value;
-    let cellSize = Math.floor(1350 / cols);
+
     for (let j = 0; j < rows; j++) {
         for (let i = 0; i < cols * cellSize; i = i + cellSize) {
+            const indexFreq = Math.floor(i * j);
+            const freq = dataArray[indexFreq];
+
             ctx.rect(i, j * cellSize, cellSize, cellSize);
-            ctx.strokeStyle = "white";
+            ctx.strokeStyle = "black";
             ctx.lineWidth = "1";
             ctx.stroke();
+            ctx.fillStyle = getColor(freq);
+            ctx.fillRect(i, j * cellSize, cellSize, cellSize);
         }
     }
 }
 
 function getColor(freq) {
     const normalFreq = (freq * 50) / 100;
-    return `hsl(${Math.floor(normalFreq)}, 100%, 20%)`;
+    return `hsl(${Math.floor(normalFreq)}, 100%, 50%)`;
 }
 
